233c2de70b792ca0bbe72c2c14589d474ca6d787
Viv696969 <chouhanvivek207@gmail.com>
2024-07-05 12:03:50 +0530

    aggregated_data

---
+++ Lok Sabha Elections Data analysis/queries/transformation.sql
@@ -0,0 +1,62 @@
+
+create view by_state_and_party_2014 as 
+select state_id,party_id,sum(general_votes) as `general_votes` ,
+sum(postal_votes) as `postal_votes`,sum(total_votes) as `total_votes`,sum(total_electors) 
+as `total_electors`
+from results_2014
+group by state_id,party_id
+order by state_id;
+
+create view by_state_and_pc_2014 as 
+select state_id,pc_id,sum(general_votes) as `general_votes` ,
+sum(postal_votes) as `postal_votes`,sum(total_votes) as `total_votes`,sum(total_electors) 
+as `total_electors`
+from results_2014
+group by state_id,pc_id
+order by state_id;
+
+select * from by_state_and_party_2014;
+
+
+create view by_state_and_category_2014 as 
+select state_id,category_id,sum(general_votes) as `general_votes` ,
+sum(postal_votes) as `postal_votes`,sum(total_votes) as `total_votes`,sum(total_electors) 
+as `total_electors`
+from results_2014
+group by state_id,category_id
+order by state_id,category_id;
+
+-- 2019
+
+create view by_state_and_party_2019 as  
+select state_id,party_id,sum(general_votes) as `general_votes` ,
+sum(postal_votes) as `postal_votes`,sum(total_votes) as `total_votes`,sum(total_electors) 
+as `total_electors`
+from results_2019
+group by state_id,party_id
+order by state_id,party_id;
+
+create view by_state_and_pc_2019 as 
+select state_id,pc_id,sum(general_votes) as `general_votes` ,
+sum(postal_votes) as `postal_votes`,sum(total_votes) as `total_votes`,sum(total_electors) 
+as `total_electors`
+from results_2019
+group by state_id,pc_id
+order by state_id;
+
+select * from by_state_and_party_2014;
+
+
+create view by_state_and_category_2019 as 
+select state_id,category_id,sum(general_votes) as `general_votes` ,
+sum(postal_votes) as `postal_votes`,sum(total_votes) as `total_votes`,sum(total_electors) 
+as `total_electors`
+from results_2019
+group by state_id,category_id
+order by state_id,category_id;
+
+
+
+
+
+
