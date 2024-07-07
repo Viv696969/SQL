f5a4c4d0f9901854ed61996b94ecde05c1effb61
Viv696969 <chouhanvivek207@gmail.com>
2024-07-07 13:14:22 +0530

    solved bug

--- Lok Sabha Elections Data analysis/queries/analysis.sql
+++ Lok Sabha Elections Data analysis/queries/analysis.sql
@@ -199,3 +199,17 @@ where party_id in (select party_id from cte_parties);
 --  select distinct party_id from cte2;
 
 
+select state_id,pc_id,sum(general_votes) as `general_votes`,sum(postal_votes) as `postal_votes`,
+sum(total_votes) as `total_votes` ,max(total_electors) as `total_electors`
+ from results_2019
+ group by state_id,pc_id
+ order by state_id,pc_id;
+
+select state_id,pc_id,sum(general_votes) ,sum(postal_votes),
+sum(total_votes)  ,max(total_electors) 
+ from results_2014
+ group by state_id,pc_id
+ order by state_id,pc_id;
+ 
+ 
+
